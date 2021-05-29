b3313395b0f1c627dd850801a4642858bc2646f7
OBEYAPATRICK <81704241+OBEYAPATRICK@users.noreply.github.com>
2021-05-29 12:24:33 +0100

    Add files via upload

---
+++ ussdcode.py
@@ -0,0 +1,46 @@
+
+print("pleas enter your USSD cod")
+option=input("")
+print("1.Check Balance\n2.Transfers\n3.Airtime\n4.Other Services")
+option=input("")
+if (option=="1"):
+    amount=input("Enter Amount")
+    pin=input("Enter pin")
+    print("Recharge successful")
+    back=input("Enter 1 To go back to menu")
+    print("Enter 2 to next")
+    option=input("")
+    if (option=="2"):
+    		amount=input("Enter Amount")
+    		number=input("Enter phone number")
+    		pin=input("Enter pin")
+    		print("recharge successful")
+    		option=input("")
+    		if (option=="3"):
+    			print("1.300mb=300N\n2.400=400N")
+    			if (option=="1"):
+    			 amount=input("Enter Amount")
+    			number=input("Enter phone number")
+    			print("Enter 1 to confrim")
+    			confirm=input("press 1")
+    			print("data recharge successful")
+    			option=input("")
+    			if (option=="4"):
+    				account_number=input("Enter the acct number")
+    			print(" the name of the account is HARUNA FLOEENCE")
+    			amount=input("Enter Amount")
+    			confirm_amount=input("Enter 1 to continue")
+    			pin=input("Enter four pin")
+    			print("transfer successful")
+    			print("thanks for banking with Access bank")
+    			option=input("")
+    			if(option=="5"):
+    				account_number=input("Enter Account Number")
+    				print("1.firstbank\n2.Uba\n3.zenith\n4.union\n5.Access\n6.Others")
+    				if(option=="1"):
+    					print("the account name is  HARUNA FLOEENCE")
+    					
+    						
+    			
+    		
+    							
